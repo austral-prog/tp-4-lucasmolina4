b911f10d19e7b1da5d887029090fefe45b3b9184
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-04-12 12:38:39 -0300

    TP4 leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,10 @@
 def leap_year():
-year= int(input("Ingrese un año:"))
-if(year % 4 == 0) and (year % 100!=0):
-   print(f"El año{year} es bisiesto")
-elif (year % 100 == 0)and (year % 400==0):
-   print(f"El año {year} es bisiesto")
-elif (year % 100 == 0) and (year % 400!=0):
-   print(f"El año {year} no es bisiesto")
-else:
- (f"El año {year} no es bisiesto")
+    year = int(input("Ingrese un año: "))
+    if (year % 4 == 0) and (year % 100 != 0):
+    	print(f"El año {year} es bisiesto")
+    elif (year % 100 == 0) and (year % 400 == 0):
+    	print(f"El año {year} es bisiesto")
+    elif (year % 100 == 0) and (year % 400 !=0):
+    	print(f"El año {year} no es bisiesto")
+    else:
+    	print(f"El año {year} no es bisiesto")

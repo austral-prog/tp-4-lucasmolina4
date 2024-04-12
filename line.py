b1a3439639bbb9bc4854f9eae790a8397991846e
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-04-12 12:27:11 -0300

    TP4 line.py

--- line.py
+++ line.py
@@ -1,20 +1,18 @@
 def line():
 import math
-coef_a= float(input("Ingrese el coeficiente A: "))
-coef_b =float(input("Ingrese el coeficiente B: "))
-coef_c =float(input("Ingrese el coeficiente C: "))
-coef_d =float(input("Ingrese el coeficiente D: "))
-
-print(f"El coeficiente A de su ecuación de la recta es:{coef_a}")
-print(f"El coeficiente B de su ecuación de la recta es:{coef_b}")
-print(f"El coeficiente X1 de su ecuación de la recta es:{coef_c}")
-print(f"El coeficiente X2 de su ecuación de la recta es:{coef_d}")
-
-print(f"Para la siguiente ecuacion:\n{coef_a}+{coef_b}")
-
-P1=print(f"Dado los siguientes numeros:\n{coef_c, coef_a * coef_c + coef_b}")
-P2=print(f"Dado los siguientes numeros:\n{coef_d, coef_a * coef_d + coef_b}")
-
-print(f"La distancia entre ellos es:" ,math.dist([coef_c, coef_a * coef_c + coef_b ], [coef_d, coef_a * coef_d + coef_b]))
-
+  coef_a = float(input("Ingrese el coeficiente A: "))
+  coef_b = float(input("Ingrese el coeficiente B: "))
+  coef_X1 = float(input("Ingrese el coeficiente X1: "))
+  coef_X2 = float(input("Ingrese el coeficiente X2: "))
+  print(f"El coeficiente A de su ecuación de la recta es: {coef_a}")
+  print(f"El coeficiente B de su ecuación de la recta es: {coef_b}")
+  print(f"El coeficiente X1 de su ecuación de la recta es: {coef_X1}")
+  print(f"El coeficiente X2 de su ecuación de la recta es: {coef_X2}")
+  print("")
+  print(f"Para la siguiente ecuación:\n\tY = {coef_a}X + {coef_b}")
+  print("")
+  print(f"Dados los siguientes puntos:\n\tP1 {coef_X1, coef_a*coef_X1 + coef_b}")
+  print(f"\tP2 {coef_X2, coef_a*coef_X2 + coef_b}")
+  print("")
+  print(f"La distancia entre ellos es:" , math.dist([coef_X1, coef_a*coef_X1 + coef_b] , [coef_X2, coef_a*coef_X2 + coef_b]))
 
